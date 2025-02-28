c50769fa4320bf562e1b482c3b6fc7b536dd4051
Ömer Faruk MERAL <omer.meral@path.com.tr>
2025-03-01 00:03:14 +0300

    refactor ParibuApp to use AppConstants and initialize ScreenSize; set SamplePage as home

--- lib/app.dart
+++ lib/app.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:paribu_mobile/core/constant/app_constant.dart';
+import 'package:paribu_mobile/core/constant/app_constants.dart';
+import 'package:paribu_mobile/core/utils/screen_size.dart';
+import 'package:paribu_mobile/feature/sample/view/sample_page.dart';
 
 class ParibuApp extends StatelessWidget {
   const ParibuApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: AppConstant.appName);
+    ScreenSize.init();
+    return MaterialApp(title: AppConstants.appName, home: SamplePage());
   }
 }
