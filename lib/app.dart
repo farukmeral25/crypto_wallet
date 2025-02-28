1d21b76573c3f15eda497eb85344d8ad82a24c8c
Ömer Faruk MERAL <omer.meral@path.com.tr>
2025-03-01 00:24:30 +0300

    add navigator key to ParibuApp for improved navigation management

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:paribu_mobile/core/constant/app_constants.dart';
 import 'package:paribu_mobile/core/constant/theme/app_theme.dart';
+import 'package:paribu_mobile/core/keys/app_keys.dart';
 import 'package:paribu_mobile/core/utils/screen_size.dart';
 import 'package:paribu_mobile/feature/sample/view/sample_page.dart';
 
@@ -10,6 +11,6 @@ class ParibuApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     ScreenSize.init();
-    return MaterialApp(theme: AppTheme.lightTheme, title: AppConstants.appName, home: SamplePage());
+    return MaterialApp(theme: AppTheme.lightTheme, title: AppConstants.appName, navigatorKey: AppKeys.navigatorKey, home: SamplePage());
   }
 }
