e4c01ab9bb71efff1c85e69bae7359abe834e05f
Ömer Faruk MERAL <omer.meral@path.com.tr>
2025-03-01 01:21:52 +0300

    add BottomNavBarCubit and BottomNavBarState for bottom navigation management

---
+++ lib/feature/bottom_nav_bar/bloc/state/bottom_nav_bar_state.dart
@@ -0,0 +1,9 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:paribu_mobile/feature/bottom_nav_bar/data/enum/bottom_nav_bar_type.dart';
+
+part 'bottom_nav_bar_state.freezed.dart';
+
+@freezed
+abstract class BottomNavBarState with _$BottomNavBarState {
+  const factory BottomNavBarState({@Default(BottomNavBarType.charts) BottomNavBarType type}) = _BottomNavBarState;
+}
