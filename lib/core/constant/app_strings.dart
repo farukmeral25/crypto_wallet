f04a463ba9283f5cbb7f71c1998a35692283017e
Ömer Faruk MERAL <omer.meral@path.com.tr>
2025-03-01 02:23:51 +0300

    update AppStrings for consistency and add new wallet-related strings

--- lib/core/constant/app_strings.dart
+++ lib/core/constant/app_strings.dart
@@ -1,5 +1,11 @@
 class AppStrings {
   static const String wallet = "Wallet";
   static const String markets = "Markets";
-  static const String createWallet = "Create Wallet";
+  static const String createWallet = "Create wallet";
+  static const String importWallet = "Import wallet";
+  static const String web3connect = "Web3 connect";
+  static const String exportWallet = "Export wallet";
+  static const String max60Characters = "Maximum 60 characters";
+  static const String pinCode = "Pin Code";
+  static const String scanQrCode = "Scan the QR code";
 }
