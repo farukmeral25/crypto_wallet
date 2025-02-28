41e5d62f090e2e511a6d3f60269fc3fce4e58647
Ömer Faruk MERAL <omer.meral@path.com.tr>
2025-03-01 00:39:05 +0300

    refactor AppIcon to allow optional color parameter with default fallback to AppColors.foregroundTertiary

--- lib/core/shared/app_icon.dart
+++ lib/core/shared/app_icon.dart
@@ -1,14 +1,15 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:paribu_mobile/core/constant/theme/app_colors.dart';
 
 class AppIcon extends StatelessWidget {
-  const AppIcon({super.key, required this.assetPath, this.height, this.width, required this.color, this.fit = BoxFit.contain});
+  const AppIcon({super.key, required this.assetPath, this.height, this.width, this.color, this.fit = BoxFit.contain});
 
   final String assetPath;
   final double? height;
   final double? width;
-  final Color color;
+  final Color? color;
   final BoxFit fit;
 
   @override
@@ -17,7 +18,7 @@ class AppIcon extends StatelessWidget {
       if (assetPath.endsWith(".svg")) {
         return SvgPicture.network(
           assetPath,
-          colorFilter: ColorFilter.mode(color, BlendMode.srcIn),
+          colorFilter: ColorFilter.mode(color ?? AppColors.foregroundTertiary, BlendMode.srcIn),
           height: height,
           width: width,
           fit: fit,
@@ -39,7 +40,7 @@ class AppIcon extends StatelessWidget {
       if (assetPath.endsWith(".svg")) {
         return SvgPicture.asset(
           assetPath,
-          colorFilter: ColorFilter.mode(color, BlendMode.srcIn),
+          colorFilter: ColorFilter.mode(color ?? AppColors.foregroundTertiary, BlendMode.srcIn),
           height: height,
           width: width,
           fit: fit,
