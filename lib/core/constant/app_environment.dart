478807d354fe71092874d214f0986492c664198a
Ömer Faruk MERAL <omer.meral@path.com.tr>
2025-03-02 23:54:03 +0300

    fix: update environment variable names for consistency

--- lib/core/constant/app_environment.dart
+++ lib/core/constant/app_environment.dart
@@ -11,6 +11,6 @@ class AppEnvironment {
 
   String get fileName => kDebugMode ? ".env.dev" : ".env.prod";
   String get domain => dotenv.get('COIN_GEKKO_DOMAIN', fallback: 'BASE_URL not found');
-  String get apiKey => dotenv.get('x_cg_demo_api_key', fallback: 'x_cg_demo_api_key not found');
+  String get apiKey => dotenv.get('X_CG_DEMO_API_KEY', fallback: 'X_CG_DEMO_API_KEY not found');
   String get walletKit => dotenv.get('WALLET_KIT', fallback: 'WALLET_KIT not found');
 }
